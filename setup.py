a43e28a0f7c9a0dd846e8b3878d1c0cd3e6fbb48
Ryan Binns <binz@binz.codes>
2016-02-07 22:53:48 +0000

    Added convert_to_e164() and dependencies.

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='numparse164',
-      version='0.1',
+      version='0.2',
       description='Parse telephone numbers',
       url='http://github.com/binzcodes/numparse164',
       author='Ryan Binns',
       author_email='binz@binz.codes',
       license='GNU',
       packages=['numparse164'],
+      install_requires=[
+          'phonenumbers',
+      ],
       zip_safe=False)
